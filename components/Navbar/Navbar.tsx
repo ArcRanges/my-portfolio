561368b2d8dd4c5c5f5f171deb56fc526cad4cfa
Arc Ranges <arc.ranges@live.ca>
2022-12-04 16:50:10 -0800

    [Navbar] Transparency on scroll

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -9,10 +9,23 @@ export default function Navbar() {
   const { authState, setAuthState } = useAuthContext();
   const [sessionSeconds, setSessionSeconds] = useState(0);
   const [loggingOut, setLoggingOut] = useState(false);
+  const [transparent, setTransparent] = useState(true);
 
   const handleLogout = () => {
     setLoggingOut(true);
   };
+
+  useEffect(() => {
+    const scrollListener = () => {
+      setTransparent(window.scrollY >= 40);
+    };
+
+    window.removeEventListener("scroll", scrollListener);
+    window.addEventListener("scroll", scrollListener, { passive: true });
+    return () => {
+      window.removeEventListener("scroll", scrollListener);
+    };
+  }, []);
 
   useEffect(() => {
     let interval: any;
@@ -43,7 +56,11 @@ export default function Navbar() {
   return (
     <nav className="fixed top-0 z-40 w-full px-5 py-3">
       <div className="container w-full mx-auto">
-        <Card className="py-2 ">
+        <Card
+          className={`py-2 transition-all duration-300 ${
+            transparent ? "bg-black" : ""
+          }`}
+        >
           <div className="flex items-center justify-between w-full">
             <div className="w-80">
               <Heading type="h4">
