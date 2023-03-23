e04db6b04c74315e84cea800562d0ac67cd6518f
Arc Ranges <arc.ranges@live.ca>
2023-03-22 22:01:12 -0700

    added ending message

--- _pages/Home/Home.tsx
+++ _pages/Home/Home.tsx
@@ -1,8 +1,10 @@
+import Icon from "components/Icon";
 import MatrixBackground from "components/MatrixBackground";
 import Section from "components/Section";
 import About from "containers/About";
 import Skills from "containers/Skills";
 import { NextPage } from "next";
+import Link from "next/link";
 import Projects from "./components/Projects";
 import Resume from "./components/Resume/Resume";
 
@@ -29,7 +31,28 @@ const Home: NextPage = () => {
         <Section>
           <Projects />
         </Section>
-
+        <Section>
+          <div className="text-center text-white">
+            <h1 className="text-4xl md:text-6xl">Thanks for visiting!</h1>
+            <h4 className="mt-2 text-gray-400">Let's connect</h4>
+            <div className="flex justify-center w-1/2 mx-auto mt-4 space-x-2 ">
+              <a
+                href="https://www.linkedin.com/in/arcranges/"
+                className="cursor-pointer"
+                target="_blank"
+              >
+                <Icon name="linkedin" className="z-10 text-4xl" />
+              </a>
+              <a
+                href="https://github.com/ArcRanges"
+                className="cursor-pointer"
+                target="_blank"
+              >
+                <Icon name="github" className="z-10 text-4xl" />
+              </a>
+            </div>
+          </div>
+        </Section>
         {/* <Section fullHeight={false}>
           <div className="text-center text-white">
             <h2 className="mb-10 text-2xl">Resume</h2>
